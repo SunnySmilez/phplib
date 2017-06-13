d152187414772ac08ef77fbed0fb08a16b23bb32
luogreen <luogreen@foxmail.com>
2017-06-13 11:34:53 +0800

    优化rsyslog日志记录方法

--- S/Log/Handler/Rsyslog.php
+++ S/Log/Handler/Rsyslog.php
@@ -6,11 +6,10 @@ class Rsyslog extends Abstraction {
 
     public function write($level, $message, $to_path) {
         $log_path = $this->getPath($level, $to_path);
-        $ident = "LOG_PATH:{$log_path}";
         $message = json_encode($message, JSON_UNESCAPED_UNICODE);
 
-        openlog($ident, LOG_ODELAY, LOG_LOCAL7);
-        $ret = syslog(LOG_DEBUG, $message);
+        openlog("", LOG_ODELAY, LOG_LOCAL7);
+        $ret = syslog(LOG_DEBUG, "[LOG_PATH:{$log_path}] {$message}");
         closelog();
 
         return $ret;
