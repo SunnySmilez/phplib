f2677741a81daef743091d716d806420cf4b4442
dongzhe.xin <dongzhe.xin@beebank.com>
2017-07-05 16:44:21 +0800

    修改查询用户路径

--- Modules/Admin/Model/Dao/Api/Auth/Auth.php
+++ Modules/Admin/Model/Dao/Api/Auth/Auth.php
@@ -11,6 +11,7 @@ use Base\Exception\Dao as Exception;
 class Auth {
 
     const PATH_AUTH = "api/auth";
+    const PATH_USER = "api/user";
 
     const ERR_CODE_SUCC = '2000000';
 
@@ -32,7 +33,7 @@ class Auth {
         );
         $token = \S\Config::confSecurity('api.auth.token');  //todo 如何存放
 
-        $resp_data = self::_request(self::PATH_AUTH . '?token=' . $token, $params);
+        $resp_data = self::_request(self::PATH_USER . '?token=' . $token, $params);
 
         return $resp_data['user_info'];
     }
